ffa474a0c711fb65656a3723c38c0123859e452e
Scarlett Clark <sgclark@kde.org>
2017-04-26 08:43:27 -0700

    rm sudo

--- setup.sh
+++ setup.sh
@@ -67,7 +67,7 @@ rbenv local 2.3.1
 bundle install
 
 sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 apt-get update
 apt-get upgrade
 
